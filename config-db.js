e1ec405e632fa04c3c84205da5cdcd2e8ca3e720
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-12-04 16:59:30 +0700

    update link diklik dan init server side rendering html

---
+++ config-db.js
@@ -0,0 +1,9 @@
+const config = {
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  connectTimeout: 60000,
+};
+
+module.exports = config;
