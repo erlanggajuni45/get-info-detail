a22231498152c9a4de85e3b6c70d3c3c2aa09046
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2023-12-04 14:05:59 +0700

    send ip, device and browser also handle cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const useragent = require('express-useragent');
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 const app = express();
 app.set('trust proxy', true);
 app.use(useragent.express());
+app.use(cors());
 
 app.get('/', (req, res) => {
-  const ipAddresses = req.ip;
-  console.log(ipAddresses);
-  console.log(req.useragent);
-  res.send(ipAddresses);
+  const ipAddresses = req.ip.replace(/^::ffff:/, '');
+  const { platform, browser } = req.useragent;
+
+  res.send({ ip: ipAddresses, device: platform, browser });
 });
 
-app.listen('5000', () => console.log('listening on port 5000'));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
